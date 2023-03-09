20616b28d11a1131d49b06e1dc6e27c1ede48780
cmaneman <48299779+cmaneman@users.noreply.github.com>
2023-03-09 00:51:39 -0500

    Add Json to the new Project version

---
+++ UnBoredV2/UnBoredV2/Controllers/JsonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnBoredV2.Controllers
+{
+    public class JsonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UnBoredV2/UnBoredV2/Models/Task.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace UnBoredV2.Models
+{
+    public class Root
+    {
+        [JsonProperty("Task")]
+        [JsonPropertyName("Task")]
+        public List<Task>? Tasks { get; set; }
+    }
+
+    public class Task //TaskData
+    {
+        [JsonProperty("task_assigned")]
+        [JsonPropertyName("task_assigned")]
+        public string? AssignedTask { get; set; }
+
+        [JsonProperty("task_type")]
+        [JsonPropertyName("task_type")]
+        public string? TaskType { get; set; }
+
+        [JsonProperty("approximate_time")]
+        [JsonPropertyName("approximate_time")]
+        public string? Time { get; set; }
+
+        [JsonProperty("difficulty")]
+        [JsonPropertyName("difficulty")]
+        public string? Difficulty { get; set; }
+
+        [JsonProperty("id")]
+        [JsonPropertyName("id")]
+        public int? IDNumber { get; set; }
+    }
+}
